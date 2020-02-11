8f551f2f67e29a5f0c9b4ff99fcba44216e8e584
Saspian <nesaspian@gmail.com>
2020-02-11 16:13:44 +0545

    GraphQL server with React view

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // schema: schema,
+    schema,
+    graphiql: true
+  })
+);
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is up and running at ${PORT}`));
